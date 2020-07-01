962daf56256fb49e588d34de1be7ed9c7c0bbd56
rodrigobenitezseb <rodrigo.seb.benitez@gmail.com>
2020-07-01 11:33:29 -0300

    Se agregan productos a la tienda

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,3 +1,4 @@
+//Lista de productos de la Tienda
 export const products = [
   {
     name: 'Phone XL',
@@ -8,6 +9,16 @@ export const products = [
     name: 'Phone Mini',
     price: 699,
     description: 'Un gran telefono con la mejor camara en el mercado'
+  },
+  {
+    name: 'Phone A20',
+    price: 299,
+    description: 'Un telefono super funcional con el mejor rendimiento'
+  },
+  {
+    name: 'Samsung A10',
+    price: 299,
+    description: 'Un telefono innovador con sistema Android'
   },
   {
     name: 'Phone Standard',
